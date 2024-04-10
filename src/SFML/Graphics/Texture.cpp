01cac32fc46eb1e15cb4240b5545d50bf57f27e9
Santiago <san@san.san>
2024-04-10 18:46:39 +0200

    mipmap changes

--- src/SFML/Graphics/Texture.cpp
+++ src/SFML/Graphics/Texture.cpp
@@ -248,7 +248,7 @@ bool Texture::loadFromStream(InputStream& stream, const IntRect& area)
 
 
 ////////////////////////////////////////////////////////////
-bool Texture::loadFromImage(const Image& image, const IntRect& area)
+bool Texture::loadFromImage(const Image& image, const IntRect& area, bool generateMipmap)
 {
     // Retrieve the image size
     int width = static_cast<int>(image.getSize().x);
@@ -262,6 +262,9 @@ bool Texture::loadFromImage(const Image& image, const IntRect& area)
         if (create(image.getSize().x, image.getSize().y))
         {
             update(image);
+
+            if (generateMipmap)
+                this->generateMipmap();
 
             return true;
         }
@@ -304,6 +307,9 @@ bool Texture::loadFromImage(const Image& image, const IntRect& area)
             // Force an OpenGL flush, so that the texture will appear updated
             // in all contexts immediately (solves problems in multi-threaded apps)
             glCheck(glFlush());
+
+            if (generateMipmap)
+                this->generateMipmap();
 
             return true;
         }
